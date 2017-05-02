95d8df7c094fba8c93e51be9b57dfd679e3d9236
Daniel Bucket <danieljbucket@gmail.com>
2017-05-01 21:32:25 -0600

    initial commit

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,26 @@
-export default class DistrictRepository {
+// import kinderData from '../data/kindergartners_in_full_day_program';
 
-}
+export default class DistrictRepository {
+  constructor(data) {
+    this.data = this.formatData(data)
+  }
+
+  formatData(raw) {
+    return raw.reduce( (formattedData, curVal) => {
+
+      !formattedData[curVal.Location] ?
+       formattedData[curVal.Location] = [] :
+       formattedData[curVal.Location].push(
+        { [curVal.TimeFrame]: curVal.Data }
+      )
+
+      return formattedData;
+    }, {})
+    console.log(baseValue);
+  };
+
+  findByName(name) {
+
+  }
+
+};
