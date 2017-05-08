7f506235f7af8c0243280cd69063593c4335ed20
Daniel Bucket <danieljbucket@gmail.com>
2017-05-08 12:05:27 -0600

    refactor code

--- test/integration/App.test.js
+++ test/integration/App.test.js
@@ -3,17 +3,8 @@ import ReactDOM from 'react-dom';
 import App from '../../src/App';
 import { shallow, mount } from 'enzyme';
 
-let app;
-let wrapperMount;
-let wrapperShallow;
 
 describe('App Test', () => {
-
-  beforeEach(() => {
-     app = new App;
-     wrapperShallow = shallow(<App />)
-     wrapperMount = mount(<App />)
-   })
 
   it('1. renders without crashing', () => {
     const div = document.createElement('div');
@@ -21,6 +12,8 @@ describe('App Test', () => {
   });
 
   it('2. should initialize with no cards to compare', () => {
+    let wrapperMount = mount(<App />)
+
     let initialLeftCard = wrapperMount.state().leftCardBlank;
     let initialRightCard = wrapperMount.state().rightCardBlank;
 
@@ -29,6 +22,8 @@ describe('App Test', () => {
   })
 
   it('3. An empty search field will show 181 cards', () => {
+    let wrapperMount = mount(<App />)
+
     let userSearch = wrapperMount.find('input');
     let searchedCardsArr = Object.keys(wrapperMount.state().districts);
 
@@ -36,6 +31,8 @@ describe('App Test', () => {
   })
 
   it('4. user searchs for "Col" and see only 2 cards', () => {
+    let wrapperMount = mount(<App />)
+
     let userSearch = wrapperMount.find('input');
 
     userSearch.simulate('change', { target: { value: 'Col' } });
